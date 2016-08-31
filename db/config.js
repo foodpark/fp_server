6cb06ccd6d4d34eb6d7b543429e276d1de775c75
Mike Peter <mpeter88@gmail.com>
2016-08-30 20:32:16 -0400

    removed test pool

--- db/config.js
+++ db/config.js
@@ -2,9 +2,5 @@ module.exports = {
   client: 'pg',
   connection: process.env.DATABASE_URL,
   directory: __dirname + '/migrations',
-  pool: {
-    min: 0,
-    max: 7
-  },
   tableName: 'migrations'
 };
