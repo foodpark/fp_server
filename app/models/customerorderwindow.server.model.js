5826e4d3365ed80bdcfc270935a5d9afdc1f6d43
Jesse <ksjn000@gmail.com>
2016-10-07 00:04:41 -0500

    Remove custom endpoint for customer order window

--- app/models/customerorderwindow.server.model.js
+++
@@ -1,9 +0,0 @@
-var knex = require ('../../config/knex');
-
-exports.getForUnit = function(id) {
-  return knex('units').select('customer_order_window').where('id', parseInt(id));
-}
-
-exports.validateUnitId = function(id) {
-  return knex('units').select('id').where('id', parseInt(id));
-}
--- app/routes/api.js
+++ app/routes/api.js
@@ -2,18 +2,12 @@ var passport = require('passport');
 var api = require('koa')();
 var config = require('../../config/config');
 var geo = require('../../app/controllers/geo.server.controller');
-var customerOrderWindow = require('../../app/controllers/customerorderwindow.server.controller');
 var Resteasy = require('koa-resteasy')(require('../../config/knex'));
 var RestOptions = require('../rest_options');
 var Router = require('koa-router');
 var geoservices = new Router();
-var cowService = new Router();
 
 geoservices.get('/mapsearch', geo.searchUnits);
-cowService.get('/customerOrderWindow', customerOrderWindow.getForUnit);
-cowService.put('/customerOrderWindow', customerOrderWindow.methodNotAllowed);
-cowService.post('/customerOrderWindow', customerOrderWindow.methodNotAllowed);
-cowService.del('/customerOrderWindow', customerOrderWindow.methodNotAllowed);
 
 api.use(passport.authenticate(['jwt','anonymous'], {session:false}));
 api.use(function *(next) {
@@ -21,7 +15,6 @@ api.use(function *(next) {
   yield next;
 });
 api.use(geoservices.routes());
-api.use(cowService.routes());
 api.use(Resteasy(RestOptions));
 
 module.exports = api;
