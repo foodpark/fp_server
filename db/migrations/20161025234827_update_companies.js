f25ed31039383563ab378574eeb60f2eb61ad5e0
Mike Peter <mpeter88@gmail.com>
2016-10-30 23:27:40 -0400

    updating column data types

--- db/migrations/20161025234827_update_companies.js
+++ db/migrations/20161025234827_update_companies.js
@@ -1,11 +1,11 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
-      knex.schema.raw('alter table companies modify tags text')
+      knex.schema.raw('alter table companies alter column tags type text')
   ]);
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-      knex.schema.raw('alter table companies modify tags json')
+      knex.schema.raw('alter table companies alter column tags type json')
   ]);
 };
