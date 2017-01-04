10be9af9db169ed395556d3ec180d0f9b3579b62
Mike Peter <mpeter88@gmail.com>
2017-01-03 22:06:13 -0500

    commenting out older code

--- app/routes/payment.server.routes.js
+++ app/routes/payment.server.routes.js
@@ -7,9 +7,11 @@ var Router = require('koa-router');
 module.exports=function(app) {
 	var router = new Router();
 
+  /*
 	router.post('/oauth/payment/token', payment.createCheckout);
 	router.get('/oauth/push/sendPush', push.sendPush);
-
+  */ 
+	
 	app.use(router.routes());
   app.use(router.allowedMethods())
 };
--- app/routes/webhook.server.routes.js
+++ app/routes/webhook.server.routes.js
@@ -7,10 +7,10 @@ var Router = require('koa-router');
 module.exports=function(app) {
 	var router = new Router();
 
-	router.post('/push/eventTrack', push.eventTrack);
+	/*router.post('/push/eventTrack', push.eventTrack);
 	router.post('/push/setDeviceToken', push.setDeviceToken);
 	router.post('/push/orderAcceptDeclice', push.orderAcceptDeclice);
-	
+	*/
 	//router.get('/oauth/push/sendPush', push.sendPush);
 
 
