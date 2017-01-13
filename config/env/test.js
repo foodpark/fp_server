2d0a9b4d43e806ac4a12112652ccb9f2cff2374d
MattGuiger <Matt@streetfoodez.com>
2017-01-13 17:33:32 -0300

    Update test.js

--- config/env/test.js
+++ config/env/test.js
@@ -9,7 +9,7 @@ module.exports = {
   clientId: "QtDrN2fxN3JGsKQvNgtahF5jz1MPP6kVV4wZTavq15",
   client_secret: "K5M1sc3PZuBVU3gn5iMzymWGecDk1HT90ZrjWVra6P",
   grant_type: "client_credentials",
-  defaultTaxBand: "1426900117021000570", // Brazil ICMS
+  defaultTaxBand: "1427064502431515521", // Brazil ICMS
   sumup:{
     clientId:"com.sumup.apisampleapp",
     client_secret:"6332ad54-eb47-4b41-8390-074f062da085",
