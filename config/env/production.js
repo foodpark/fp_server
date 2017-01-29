49afe0527607e03650f06e4d5a270e055a3de487
Michael Peter <mpeter88@gmail.com>
2017-01-29 17:32:57 -0500

    resetting default tax band

--- config/env/production.js
+++ config/env/production.js
@@ -9,7 +9,7 @@ module.exports = {
   clientId: "",
   client_secret: "",
   grant_type: "client_credentials",
-  defaultTaxBand: "1278235843793780901", // Brazil ICMS
+  defaultTaxBand: "1427064502431515521", // Brazil ICMS
   deliveryCharge: "",
   sumup:{
       clientId:"",
