4211b92516e4f7630854975a25ab81d37d6cbc8b
Thomas Batzold <nf11624@gmail.com>
2017-06-07 23:55:14 -0400

    Remove mapping.email for username
    Change mapping.email to mapping.username

--- app/controllers/authentication.server.controller.js
+++ app/controllers/authentication.server.controller.js
@@ -586,7 +586,7 @@ exports.fbLogin = function*() {
       mapping.username = fb_email;
     }
     else {
-      mapping.email = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      mapping.username = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
           var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
           return v.toString(16);
       }) + '@sfez.com';
