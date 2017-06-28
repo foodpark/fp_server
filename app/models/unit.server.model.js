c47deff68811dd082a55c3bd66321456cb4f3faa
Adam Vainder <adamvainder@gmail.com>
2017-06-27 23:56:17 -0400

    add company photo for mapsearch query

--- app/models/unit.server.model.js
+++ app/models/unit.server.model.js
@@ -27,7 +27,7 @@ exports.findByCheckinTimebox = function(latitude, longitude, distance, searchtim
     var minlon = lon2;
     var maxlon = lon1;
   }
-  return knex('checkins').select(['units.id as id','units.name','companies.name as company_name','units.number','units.customer_order_window','units.territory_id','units.type','units.description','units.qr_code','units.unit_order_sys_id','units.delivery','checkins.latitude','checkins.longitude','checkins.company_id','checkins.check_in','checkins.check_out','checkins.food_park_name','checkins.food_park_id','companies.tags','companies.calculated_rating as rating'])
+  return knex('checkins').select(['units.id as id','units.name','companies.name as company_name','units.number','units.customer_order_window','units.territory_id','units.type','units.description','units.qr_code','units.unit_order_sys_id','units.delivery','checkins.latitude','checkins.longitude','checkins.company_id','checkins.check_in','checkins.check_out','checkins.food_park_name','checkins.food_park_id','companies.tags','companies.calculated_rating as rating','companies.photo'])
   .whereRaw('(latitude >= ? and latitude <= ?) and (longitude >= ? and longitude <= ?) and check_in < ? and check_out > ?',
   [minlat, maxlat, minlon, maxlon, searchtime, searchtime]).innerJoin('units','units.id','checkins.unit_id').innerJoin('companies','companies.id','checkins.company_id');//.on('query-response', function(response, obj, builder) {});
   //.then( function(response) {
