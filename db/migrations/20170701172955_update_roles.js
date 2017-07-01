0e8369cf8ed6fe04117ec35010ef5f560c88b702
Adam Vainder <adamvainder@gmail.com>
2017-07-01 18:30:41 -0400

    teardown for role update migration

--- db/migrations/20170701172955_update_roles.js
+++ db/migrations/20170701172955_update_roles.js
@@ -4,3 +4,9 @@ exports.up = function(knex, Promise) {
           '(5,\'DRIVER\');' )
   ]);
 };
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+        knex.raw('delete from roles where id=5;' )
+  ]);
+};
