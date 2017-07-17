8c1f767120d9c2bc5c1687dd9e651148ffcbe40d
Madan <clickripple.master@gmail.com>
2017-07-17 09:52:06 +0530

    saving_territorid_and_countryid

--- app/rest_options.js
+++ app/rest_options.js
@@ -1165,7 +1165,7 @@ function *beforeSaveDriver() {
     }
     else if (createUser){
       debug('..user' + createUser);
-      var driver={ role: 'DRIVER', first_name:this.resteasy.object.name, last_name:createUser.last_name, username:createUser.username, password:createUser.password };
+      var driver={ role: 'DRIVER', first_name:this.resteasy.object.name, last_name:createUser.last_name, username:createUser.username, password:createUser.password, territory_id:createUser.territory_id, phone:createUser.phone };
       delete this.resteasy.object.user; //remove user object from the main objectbecause it's not a real domian field
       var newUser='';
       try{  
