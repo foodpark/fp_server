20a637e6e84e5f863f948441f43db750996588eb
Jonatas Pedraza <jonatas.nona@gmail.com>
2017-11-04 23:39:50 -0200

    Remove unnecessary code

--- app/controllers/foodpark.server.controller.js
+++ app/controllers/foodpark.server.controller.js
@@ -7,22 +7,6 @@ var debug = require('debug')('storefront');
 var _ = require('lodash');
 var logger = require('winston');
 
-const getErrorMessage = (err) => {
-  let message = '';
-  for (let errName in err.errors) {
-    if (err.errors[errName].message)
-      message = err.errors[errName].message;
-  }
-  return message;
-};
-
-const sendErrorResponse = (err, res, status) => {
-  if (!status) status = 500
-  return res.status(status).send({
-    'error': err
-  })
-};
-
 exports.getFoodParkCheckins = function * (id, next) {
   var user = this.passport.user
 
