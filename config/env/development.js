06a1accbfb390458ea1e1b4e80b41bb374a61a37
jacncarvalho <jacncarvalho@gmail.com>
2017-11-15 21:51:24 -0200

    adding delivery charges arrays

--- config/env/development.js
+++ config/env/development.js
@@ -11,7 +11,8 @@ module.exports = {
   grant_type: "client_credentials",
   olddefaultTaxBand: "1278235843793780901", // Brazil ICMS
   defaultTaxBand: "1427064502431515521",
-  deliveryCharge: "10",
+  deliveryRadius: ["2", "10", "15"],
+  deliveryCharge: ["0", "8", "10"],
   deliveryOffset: 15,
   sumup:{
     clientId:"com.sumup.apisampleapp",
