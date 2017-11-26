de20b47899c4259334bc368c292614a771785529
Sávio Muniz <saviofelixmuniz@gmail.com>
2017-11-26 12:42:37 -0300

    changing query

--- app/models/foodpark.server.model.js
+++ app/models/foodpark.server.model.js
@@ -32,7 +32,7 @@ exports.setManager = function (foodParkId, userId) {
 };
 
 exports.getAllDrivers = function (foodParkId) {
-  return knex.raw(`select users.* from drivers_foodpark df right join users on df.user_id = users.id where f.food_park_id = ${foodParkId};`);
+  return knex.raw(`select users.* from drivers_foodpark df right join users on df.user_id = users.id where df.food_park_id = ${foodParkId};`);
 };
 
 exports.addDriver = function (driverFoodpark) {
