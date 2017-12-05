483c2cde1608f3e298de1567aa744fa42f29e14b
Sávio Muniz <saviofelixmuniz@gmail.com>
2017-12-04 23:52:32 -0300

    en us for script

--- scripts/fetchdata.sh
+++ scripts/fetchdata.sh
@@ -13,7 +13,7 @@ sudo -u postgres psql -U postgres  -c "DROP ROLE sfez_rw;"
 sudo -u postgres psql -U postgres  -c "CREATE USER sfez_rw WITH PASSWORD 'sfez';"
 
 sudo -u postgres psql -U postgres  -c "CREATE DATABASE sfezdb WITH OWNER = postgres ENCODING = 'UTF8' TABLESPACE = pg_default \
-LC_COLLATE = 'pt_BR.UTF-8' LC_CTYPE = 'pt_BR.UTF-8' CONNECTION LIMIT = -1;"
+LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8' CONNECTION LIMIT = -1;"
 
 sudo -u postgres psql -U postgres  -c "GRANT CONNECT ON DATABASE sfezdb TO sfez_rw;"
 
