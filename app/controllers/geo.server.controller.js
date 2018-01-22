c958fab6fc2bcb89b5d40e65c9715454ad0b74d4
Sávio Muniz <saviofelixmuniz@gmail.com>
2018-01-22 17:12:16 -0300

    venue type on map search

--- app/controllers/geo.server.controller.js
+++ app/controllers/geo.server.controller.js
@@ -1,4 +1,5 @@
 var Unit = require ('../models/unit.server.model');
+var FoodParks = require ('../models/foodpark.server.model');
 var debug = require('debug')('geo.server.controller');
 var logger = require('winston');
 
@@ -15,6 +16,7 @@ exports.searchUnits=function *(next) {
 	meta.date=date;
 	var units = Unit.findByCheckinTimebox(parseFloat(this.query.latitude), parseFloat(this.query.longitude), parseFloat(this.query.distance), date).then( function (result) {
 		debug(result);
+
 		logger.info('Unit checkins found', meta);
 		return result;
 	}).catch(
@@ -23,7 +25,33 @@ exports.searchUnits=function *(next) {
 			logger.error('error while searching for unit checkins', meta);
 			throw err;
 		});
-	this.body = yield units;
+
+	var resultUnits = yield units;
+
+	var promises = [];
+
+	resultUnits.forEach( function (unit, index) {
+	    if (unit.food_park_id) {
+        promises.push(FoodParks.getSingleFoodPar(unit.food_park_id));
+      }
+      else {
+	      promises.push(undefined);
+      }
+  });
+
+	var response = Promise.all(promises).then(function (result) {
+    result.forEach(function (foodPark, index) {
+      if (foodPark)
+        resultUnits[index].venue_type = foodPark[0].type;
+      else
+        resultUnits[index].venue_type = null;
+    });
+
+    return resultUnits
+  });
+
+  this.body = yield response;
+
 };
 
 exports.searchPostal=function *(next) {
